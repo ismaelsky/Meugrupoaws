3debb203691c0da459aab5d9ea2840e065821234
ismaelsky <ismael.sky1@gmail.com>
2019-12-26 20:44:24 -0300

    data()...
    arquivos recebido do get armazenados data('auth').

--- page/login/script.js
+++ page/login/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
   $('#auth').click(function(){
-    alert('1');
+    //alert('1');
     var login = $('#last_login').val();
     var pass = $('#last_pass').val();
 
@@ -10,51 +10,7 @@ $(document).ready(function () {
       $.each(data, function (index, user) {
         if(user.auth === 'ok'){
 
-          //--------------------------------------------------------------------------
-
-          var app = {
-            // Application Constructor
-            initialize: function() {
-              document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-            },
-
-            // deviceready Event Handler
-            //
-            // Bind any cordova events here. Common events are:
-            // 'pause', 'resume', etc.
-            onDeviceReady: function() {
-              this.receivedEvent('deviceready');
-            },
-
-            // Update DOM on a Received Event
-            receivedEvent: function(id) {
-              alert('2');
-              if (window.cordova.platformId === "browser") db = window.openDatabase('MeuGrupoSqLite', '1.0', 'Data', 2*1024*1024);
-              else db = window.sqlitePlugin.openDatabase({name: 'MeuGrupoSqLite.db', location: 'default'});
-              alert('3');
-              db.transaction(function(tx) {
-
-                var create_banco = "CREATE TABLE IF NOT EXISTS `tbUser` (`idUser`, `stgNome`, `stgUserName`, `stgEmail`, `stgTelefone`, `stgPass`, `intNivel`, `stgEndereco`, `Img`, `stgAfiliacao`, `stgCodAux`);";
-
-                tx.executeSql(create_banco);
-                tx.executeSql('INSERT INTO tbUser VALUES (idUser,stgNome,stgUserName,stgEmail,stgTelefone,stgPass,intNivel,stgEndereco,Img,stgAfiliacao)', [1,user.stgNome, user.stgUserName,user.stgEmail,user.stgTelefone,user.stgPass,user.intNivel,user.stgEndereco,user.Img,user.stgAfiliacao]);
-
-              }, function(error) {
-                alert('Transaction ERROR: ' + error.message);
-              }, function() {
-                alert('Populated database OK');
-              });
-
-
-            }
-
-
-          };
-
-          app.initialize();
-
-          //--------------------------------------------------------------------------
-
+          $( "body" ).data( { auth: [ user.idUser,user.stgNome,user.stgUserName,user.stgEmail,user.stgTelefone,user.stgPass,user.intNivel,user.stgEndereco,user.Img,user.stgAfiliacao]} );
 
         }else{
           alert(user.auth);
@@ -67,3 +23,60 @@ $(document).ready(function () {
 
 
 });//end-ready
+
+
+$('#rec_euth').click(function(){
+  var all = $( "body" ).data('auth');
+  $.each(all, function (index, user) {
+    alert(user);
+  });
+});//end-click
+
+//--------------------------------------------------------------------------
+
+var app = {
+  // Application Constructor
+  initialize: function() {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  },
+
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function() {
+    this.receivedEvent('deviceready');
+  },
+
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    alert('2');
+    if (window.cordova.platformId === "browser") db = window.openDatabase('MeuGrupoSqLite', '1.0', 'Data', 2*1024*1024);
+    else db = window.sqlitePlugin.openDatabase({name: 'MeuGrupoSqLite.db', location: 'default'});
+    alert('3');
+    db.transaction(function(tx) {
+
+
+
+      var create_banco = "CREATE TABLE IF NOT EXISTS `tbUser` (`idUser`, `stgNome`, `stgUserName`, `stgEmail`, `stgTelefone`, `stgPass`, `intNivel`, `stgEndereco`, `Img`, `stgAfiliacao`, `stgCodAux`);";
+
+      tx.executeSql(create_banco);
+      tx.executeSql('INSERT INTO tbUser VALUES (idUser,stgNome,stgUserName,stgEmail,stgTelefone,stgPass,intNivel,stgEndereco,Img,stgAfiliacao)', [1,user.stgNome, user.stgUserName,user.stgEmail,user.stgTelefone,user.stgPass,user.intNivel,user.stgEndereco,user.Img,user.stgAfiliacao]);
+
+
+
+    }, function(error) {
+      alert('Transaction ERROR: ' + error.message);
+    }, function() {
+      alert('Populated database OK');
+    });
+
+
+  }
+
+
+};
+
+app.initialize();
+
+//--------------------------------------------------------------------------
